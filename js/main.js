d2af54b96ac7ca213a00fd8ddbce26b1ef344265
camgelozzi <cangelozzi@gmail.com>
2018-10-17 11:50:42 -0400

    fetch JS initial setup update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 // FUNCTIONS
 function fetchData() {
-  fetch('../../authoring_w7/includes/connect.php?carModel=R58')
+  fetch('./includes/connect.php?carModel=R58')
   .then(res => res.json())
   .then(data => console.log(data)) 
   .catch(function(error) {
